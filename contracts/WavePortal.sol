dc0aca9210589bfe5d204709cb08610f603025c6
Carlos Henrique <ch.henriquevinha@gmail.com>
2023-01-02 10:20:11 -0300

    feat: store data on smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,8 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("Hi! I'm your first smart contract :)");
+      console.log("Hi! I'm your first smart contract :)");
+    }
+
+    function wave() public {
+      totalWaves += 1;
+      console.log("%s deixou uma mensagem!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+      console.log("Temos um total de %d mensagens!", totalWaves);
+      return totalWaves;
     }
 }
 
